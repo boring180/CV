1509a7f174251d8bca89d65d36b317e9ce489ac8
Borong <borongxu@outlook.com>
2025-11-19 21:47:08 +0800

    Add Chinese

--- compile.sh
+++ compile.sh
@@ -1,1 +1,13 @@
-lualatex cv.tex
+#!/bin/bash
+# Compile script for CV
+# Usage: ./compile.sh [ch|en]
+#   ch - compile Chinese version (cv_ch.tex)
+#   en - compile English version (cv.tex) [default]
+
+if [ "$1" = "ch" ]; then
+    echo "Compiling Chinese version (cv_ch.tex)..."
+    lualatex cv_ch.tex
+else
+    echo "Compiling English version (cv.tex)..."
+    lualatex cv.tex
+fi
